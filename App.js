a1ebab9f30e090ae9ea0b373dceb6e838e9b15ca
Pavneet-Sing <dev.pavneet@gmail.com>
2020-08-05 01:38:01 +0530

    implement counter

--- App.js
+++ App.js
@@ -1,24 +1,48 @@
 /**
  * @author Pavneet Singh
- * 
+ * Click-Counter React Native App
+ *
  * @format
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   StatusBar,
+  Button,
+  View
 } from 'react-native';
 
 const App: () => React$Node = () => {
+
+  const [count, setCount] = useState(0);
+
+  const counterPlus = () => {
+    setCount(count + 1 <= Number.MAX_SAFE_INTEGER ? count + 1 : count)
+  }
+
+  const counterMinus = () => {
+    setCount(count - 1 >= Number.MIN_SAFE_INTEGER ? count - 1 : count)
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.container}>
-        <Text style={styles.center} >Hello World!</Text>
+        <Text style={styles.textCounter} >{count}</Text>
+        <View style={styles.buttonStyle}>
+          <Button
+            onPress={counterPlus}
+            title='+' />
+        </View>
+        <View style={styles.buttonStyle}>
+          <Button
+            onPress={counterMinus}
+            title='-' />
+        </View>
       </SafeAreaView>
     </>
   );
@@ -31,10 +55,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#e6e6fa',
   },
-  center: {
+  textCounter: {
     fontSize: 28,
     color: '#000',
   },
+  buttonStyle: {
+    width: "80%",
+    margin: 10,
+  }
 });
 
 export default App;
